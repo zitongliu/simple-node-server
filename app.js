206ccfb4eff7d714e07b86c5c6198bcf3b16a92c
zitongliu <zitongliu7@gmail.com>
2019-04-12 11:18:14 +1000

    refactored routes into seperate file

--- app.js
+++ app.js
@@ -1,49 +1,15 @@
 // core modules in node include http, https, fs, path, os etc
 const http = require('http');
-const fs = require('fs');
+const routes = require('./routes');
 
 // request listener
-function rqListener(req, res) {
-    // console.log(req.url, req.method, req.headers);
-    // process.exit();
+// function rqListener(req, res) {
+//     console.log(req.url, req.method, req.headers);
+//     process.exit();
+// }
 
-    const url = req.url;
-    const method = req.method;
+console.log(routes.someText);
 
-    if (url === '/') {
-        res.setHeader('Content-Type', 'text/html');
-        res.write('<html>');
-        res.write('<head>Enter Message</head>');
-        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form>');
-        res.write('</html>');
-        return res.end();
-    }
-
-    if (url === '/message' && method === 'POST') {
-        const body = [];
-        req.on('data', (chunk) => {
-            console.log(chunk);
-            body.push(chunk);
-        });
-        return req.on('end', () => {
-            const parsedBody = Buffer.concat(body).toString();
-            const message = parsedBody.split('=')[1];
-            fs.writeFile('message.txt', message, err => {
-                res.statusCode = 302;
-                res.setHeader('Location', '/');
-                res.end();
-            });
-        });
-    }
-
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<html>');
-    res.write('<head>My First Page</head>');
-    res.write('<body><h1>Hello from my Node.js server!</h1></body>')
-    res.write('</html>');
-    res.end();
-}
-
-const server = http.createServer(rqListener);
+const server = http.createServer(routes.handler);
 
 server.listen(3000);
